f77f0e845ca3313aa1df40fd785a05d1714ae854
Farshad Mousalou <farshadm90@gmail.com>
2020-11-27 10:30:44 +0330

    change layout from empty layout to basic layout.

--- app/src/main/java/me/ifarshad/rcycleviewapplication/MainActivity.kt
+++ app/src/main/java/me/ifarshad/rcycleviewapplication/MainActivity.kt
@@ -1,20 +1,25 @@
 package me.ifarshad.rcycleviewapplication
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
+    lateinit var fab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
         setupRecycleView()
-
+        setupFabButton()
     }
 
     private fun setupRecycleView() {
@@ -22,5 +27,15 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = ListRecyclerViewAdapter()
     }
+
+    private fun setupFabButton() {
+        this.fab = findViewById(R.id.fab)
+        this.fab.setOnClickListener {
+            Snackbar.make(it, "This is main activity", Snackbar.LENGTH_LONG)
+                .setAction("Dismiss", {})
+                .show()
+        }
+    }
+
 
 }
